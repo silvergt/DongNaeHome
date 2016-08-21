e4d23a34f97af74525732ea8f8f63da33c1bf9a6
junyeoulyang <silvergt@naver.com>
2016-08-21 22:59:06 +0900

    recommendation status updated

---
+++ src/TempData.java
@@ -0,0 +1,31 @@
+/**
+ * Created by junyeoulyang on 8/8/16.
+ */
+public class TempData {
+    /*
+
+    ********* themeIsChecked
+                        0           1           2           3           4           5
+    1   학교행사 :		    교내행사		학회,동아리
+    2   전시, 공연:	    전시,미술		뮤지컬		연극		    아동,가족		콘서트,공연	축제
+    3   설명회, 공모전:	    기업설명회		공모전		기업행사	    학생 설명회
+    4   구인구직:		    아르바이트		공채, 특채
+
+    ********* regionIsChecked
+
+                    0    1    2    3    4   5    6    7    8    9   10   11   12   13   14   15   16
+    지역: 		    서울, 경기, 인천, 부산, 대구, 울산, 대전, 광주, 경남, 경북, 전남, 전북, 충남, 충북, 강원, 세종, 제주
+    */
+
+
+    final static int STATUS_RECOMMENDATION=-100;
+    final static int STATUS_POSTER_FULL = -200;
+    final static int STATUS_POSTER_ABBREVIATED = -300;
+
+    final static int ORDER_DATE=110;
+    final static int ORDER_LIKE=120;
+    final static int ORDER_VISIT=130;
+
+
+
+}
--- src/maptest.java
+++ src/maptest.java
@@ -11,6 +11,7 @@ import pre_Poster.Pre_Poster;
 public class maptest {
     static int serverCount=0;
     final static int totalServerAccept=10;
+
     public static void main(String[] args){
         maptest serv=new maptest();
         serv.server();
@@ -24,32 +25,121 @@ public class maptest {
                 if(threadCounterAndAccepter(true)){
                     try {
                         Thread th = new Thread(new Runnable() {
+                            int status;
+                            int order;
+                            String search;
+                            int startNum;
+                            boolean[] filterState;
+
+                            private boolean retrieveInformationFrom(ArrayList<Object> obj){
+                                try {
+                                    status = (int) obj.get(0);
+                                    order = (int) obj.get(1);
+                                    search = obj.get(2).toString();
+                                    startNum = (int) obj.get(3);
+                                    filterState = (boolean[]) obj.get(4);
+                                    return true;
+                                }catch (Exception e){
+                                    System.out.println("retrieving information failed!!");
+                                    return false;
+                                }
+
+                            }
+
+
+                            private ArrayList<Pre_Poster> postersFromSTATUS_RECOMMENDATION(){
+                                ArrayList<Pre_Poster> posterList=new ArrayList<>();
+                                int[] test = {1, 2, 3};
+                                Pre_Poster post = new Pre_Poster(1, "a", test, test, 1000, 124, "http://cafeptthumb1.phinf.naver.net/20101214_30/qaz9949_1292263897384EE4cK_jpg/%B2%DC%B9%FA%C7%CF%C4%A1%C0%C7%B4%EB%B8%F0%C7%E81_qaz9949.jpg?type=w740"
+                                        , test, 100, 1413);
+                                Pre_Poster post2 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://imgnews.naver.net/image/382/2015/07/29/72768009.2_99_20150729145527.jpg?type=w540"
+                                        , test, 100, 1413);
+                                Pre_Poster post3 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://cafeptthumb3.phinf.naver.net/20150328_180/comic567_1427493620200sEkh1_JPEG/%B4%D9%C0%CC%B3%EB_%C5%B8%C0%D3_main_poster.jpg?type=w740"
+                                        , test, 100, 1413);
+                                Pre_Poster post4 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://mimgnews1.naver.net/image/009/2011/10/19/0090002556687_0.jpg?type=w540"
+                                        , test, 100, 1413);
+                                Pre_Poster post5 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://dbscthumb.phinf.naver.net/0903_000_2/20120314151219525_6Y6JPKNKN.jpg/he1_733_i1.jpg?type=w492_2&wm=Y"
+                                        , test, 100, 1413);
+
+                                posterList.add(post);
+                                posterList.add(post2);
+                                posterList.add(post3);
+                                posterList.add(post4);
+                                posterList.add(post5);
+                                return posterList;
+                            }
+
+                            private ArrayList<Pre_Poster> postersFromSTATUS_POSTER_ABBREVIATED(){
+                                ArrayList<Pre_Poster> posterList=new ArrayList<>();
+
+        //******TEMPORARY METHOD!!!!!!
+                                int[] test = {1, 2, 3};
+                                Pre_Poster post = new Pre_Poster(1, "a", test, test, 1000, 124, "http://cafeptthumb1.phinf.naver.net/20101214_30/qaz9949_1292263897384EE4cK_jpg/%B2%DC%B9%FA%C7%CF%C4%A1%C0%C7%B4%EB%B8%F0%C7%E81_qaz9949.jpg?type=w740"
+                                        , test, 100, 1413);
+                                Pre_Poster post2 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://imgnews.naver.net/image/382/2015/07/29/72768009.2_99_20150729145527.jpg?type=w540"
+                                        , test, 100, 1413);
+                                Pre_Poster post3 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://cafeptthumb3.phinf.naver.net/20150328_180/comic567_1427493620200sEkh1_JPEG/%B4%D9%C0%CC%B3%EB_%C5%B8%C0%D3_main_poster.jpg?type=w740"
+                                        , test, 100, 1413);
+                                Pre_Poster post4 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://mimgnews1.naver.net/image/009/2011/10/19/0090002556687_0.jpg?type=w540"
+                                        , test, 100, 1413);
+                                Pre_Poster post5 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://dbscthumb.phinf.naver.net/0903_000_2/20120314151219525_6Y6JPKNKN.jpg/he1_733_i1.jpg?type=w492_2&wm=Y"
+                                        , test, 100, 1413);
+
+                                posterList.add(post);
+                                posterList.add(post2);
+                                posterList.add(post3);
+                                posterList.add(post4);
+                                posterList.add(post5);
+                                posterList.add(post5);
+                                posterList.add(post5);
+                                posterList.add(post5);
+        //******TEMPORARY METHOD!!!!!!
+
+                                return posterList;
+                            }
+
+                            private ArrayList<Pre_Poster> postersFromSTATUS_POSTER_FULL(){
+                                ArrayList<Pre_Poster> posterList=new ArrayList<>();
+
+                                return posterList;
+                            }
+
+
+                            private ArrayList<Pre_Poster> makePosterLists(){
+                                if(status==TempData.STATUS_RECOMMENDATION){
+                                    return postersFromSTATUS_RECOMMENDATION();
+                                }else if(status==TempData.STATUS_POSTER_ABBREVIATED){
+                                    return postersFromSTATUS_POSTER_ABBREVIATED();
+                                }else if(status==TempData.STATUS_POSTER_FULL){
+                                    return postersFromSTATUS_POSTER_FULL();
+                                }
+
+                                return null;
+                            }
+
+
 
                             @Override
                             public void run() {
                                 try {
-                                    //System.out.println(serverCount);
                                     Socket socket = server.accept();
                                     System.out.println("GOTCHA!");
                                     threadCounterAndAccepter(false);
-                                    
-                                    //receive from client's method "sendCurrentStatus"
 
                                     InputStream IS = socket.getInputStream();
                                     ObjectInputStream OIS = new ObjectInputStream(IS);
 
-                                    ArrayList<Object> obj = (ArrayList<Object>) OIS.readObject();
-                                    System.out.println((int) obj.get(0));
-                                    System.out.println((int) obj.get(1));
-                                    System.out.println(obj.get(2).toString());
-                                    System.out.println((int) obj.get(3));
-                                    boolean[] bol=(boolean[])obj.get(4);
-                                    if(bol!=null) {
-                                        for (int i = 0; i < bol.length;i++) {
-                                            System.out.print("*** "+bol[i]+" ");
-                                        }
+                                    if(!retrieveInformationFrom((ArrayList<Object>) OIS.readObject())){
+                                        System.out.println("retrieving failed... closing socket!");
+                                        //******SEND FAILURE STATUS POSTER TO CLIENT HERE!
+                                        OIS.close();
+                                        IS.close();
+                                        socket.close();
+                                        return;
                                     }
 
+
+                                    System.out.println("receive completed from socket - " + socket.toString());
 
 
 
@@ -58,35 +148,10 @@ public class maptest {
 
                                     //******search proper poster data and make 'posters' variable
 
-
-
-
-                                    int[] test = {1, 2, 3};
-                                    Pre_Poster post = new Pre_Poster(1, "a", test, test, 1000, 124, "http://cafeptthumb1.phinf.naver.net/20101214_30/qaz9949_1292263897384EE4cK_jpg/%B2%DC%B9%FA%C7%CF%C4%A1%C0%C7%B4%EB%B8%F0%C7%E81_qaz9949.jpg?type=w740"
-                                            , test, 100, 1413);
-                                    Pre_Poster post2 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://imgnews.naver.net/image/382/2015/07/29/72768009.2_99_20150729145527.jpg?type=w540"
-                                            , test, 100, 1413);
-                                    Pre_Poster post3 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://cafeptthumb3.phinf.naver.net/20150328_180/comic567_1427493620200sEkh1_JPEG/%B4%D9%C0%CC%B3%EB_%C5%B8%C0%D3_main_poster.jpg?type=w740"
-                                            , test, 100, 1413);
-                                    Pre_Poster post4 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://mimgnews1.naver.net/image/009/2011/10/19/0090002556687_0.jpg?type=w540"
-                                            , test, 100, 1413);
-                                    Pre_Poster post5 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://dbscthumb.phinf.naver.net/0903_000_2/20120314151219525_6Y6JPKNKN.jpg/he1_733_i1.jpg?type=w492_2&wm=Y"
-                                            , test, 100, 1413);
-
-                                    Pre_Poster[] p=new Pre_Poster[8];
-                                    p[0]=post;
-                                    p[1]=post2;
-                                    p[2]=post3;
-                                    p[3]=post4;
-                                    p[4]=post5;
-                                    p[5]=post5;
-                                    p[6]=post5;
-                                    p[7]=post5;
-
                                     OutputStream OS = socket.getOutputStream();
                                     ObjectOutputStream OOS = new ObjectOutputStream(OS);
 
-                                    OOS.writeObject(p);
+                                    OOS.writeObject(makePosterLists());
 
 
                                     System.out.println("END!");
@@ -137,5 +202,6 @@ public class maptest {
         System.out.println(serverCount);
         return true;
     }
+
 }
 
