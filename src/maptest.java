291cae1faa3325ecd7a2c73117b307d08a871a30
junyeoulyang <silvergt@naver.com>
2016-08-22 18:20:14 +0900

    recommendation poster changed(temp)

--- src/maptest.java
+++ src/maptest.java
@@ -49,16 +49,18 @@ public class maptest {
 
                             private ArrayList<Pre_Poster> postersFromSTATUS_RECOMMENDATION(){
                                 ArrayList<Pre_Poster> posterList=new ArrayList<>();
+
+                                //******TEMPORARY METHOD!!!!!!
                                 int[] test = {1, 2, 3};
-                                Pre_Poster post = new Pre_Poster(1, "a", test, test, 1000, 124, "http://cafeptthumb1.phinf.naver.net/20101214_30/qaz9949_1292263897384EE4cK_jpg/%B2%DC%B9%FA%C7%CF%C4%A1%C0%C7%B4%EB%B8%F0%C7%E81_qaz9949.jpg?type=w740"
+                                Pre_Poster post = new Pre_Poster(1, "a", test, test, 1000, 124, "http://dbscthumb.phinf.naver.net/0903_000_2/20120314151219525_6Y6JPKNKN.jpg/he1_733_i1.jpg?type=w492_2&wm=Y"
                                         , test, 100, 1413);
-                                Pre_Poster post2 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://imgnews.naver.net/image/382/2015/07/29/72768009.2_99_20150729145527.jpg?type=w540"
+                                Pre_Poster post2 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://dthumb.phinf.naver.net/?src=%22http%3A%2F%2Fdbscthumb.phinf.naver.net%2F3329_000_25%2F20140925020328314_I5DE0YIR5.jpg%2F05-509391.jpg%3Ftype%3Dw690_fst_n%26wm%3DY%22&twidth=690&theight=509&opts=17"
                                         , test, 100, 1413);
-                                Pre_Poster post3 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://cafeptthumb3.phinf.naver.net/20150328_180/comic567_1427493620200sEkh1_JPEG/%B4%D9%C0%CC%B3%EB_%C5%B8%C0%D3_main_poster.jpg?type=w740"
+                                Pre_Poster post3 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://postfiles3.naver.net/20150715_82/1st_realize_1436938769715fLGLx_JPEG/%B1%B9%B0%A1%BA%B0_%B8%F0%BA%B4%C6%F7%BD%BA%C5%CD9.jpg?type=w1"
                                         , test, 100, 1413);
-                                Pre_Poster post4 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://mimgnews1.naver.net/image/009/2011/10/19/0090002556687_0.jpg?type=w540"
+                                Pre_Poster post4 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://postfiles13.naver.net/20150715_124/1st_realize_1436938770309nnh5g_JPEG/%B1%B9%B0%A1%BA%B0_%B8%F0%BA%B4%C6%F7%BD%BA%C5%CD6.jpg?type=w1"
                                         , test, 100, 1413);
-                                Pre_Poster post5 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://dbscthumb.phinf.naver.net/0903_000_2/20120314151219525_6Y6JPKNKN.jpg/he1_733_i1.jpg?type=w492_2&wm=Y"
+                                Pre_Poster post5 = new Pre_Poster(1, "a", test, test, 1000, 124, "http://postfiles12.naver.net/20130216_43/suney_k0304_1360991836913Dxvhv_JPEG/1_%284%29.jpg?type=w2"
                                         , test, 100, 1413);
 
                                 posterList.add(post);
@@ -66,6 +68,10 @@ public class maptest {
                                 posterList.add(post3);
                                 posterList.add(post4);
                                 posterList.add(post5);
+
+                                //******TEMPORARY METHOD!!!!!!
+
+                                System.out.println("returning RECOMM poster");
                                 return posterList;
                             }
 
@@ -94,6 +100,7 @@ public class maptest {
                                 posterList.add(post5);
                                 posterList.add(post5);
         //******TEMPORARY METHOD!!!!!!
+                                System.out.println("returning ABBR poster");
 
                                 return posterList;
                             }
@@ -101,6 +108,8 @@ public class maptest {
                             private ArrayList<Pre_Poster> postersFromSTATUS_POSTER_FULL(){
                                 ArrayList<Pre_Poster> posterList=new ArrayList<>();
 
+
+                                System.out.println("returning FULL poster");
                                 return posterList;
                             }
 
@@ -139,7 +148,8 @@ public class maptest {
                                     }
 
 
-                                    System.out.println("receive completed from socket - " + socket.toString());
+                                    System.out.println("receive completed from socket - " + socket.toString() +" $ "+Integer.toString(status)
+                                            +" $ "+Integer.toString(order)+" $ "+search+" $ "+Integer.toString(startNum));
 
 
 
@@ -150,6 +160,7 @@ public class maptest {
 
                                     OutputStream OS = socket.getOutputStream();
                                     ObjectOutputStream OOS = new ObjectOutputStream(OS);
+
 
                                     OOS.writeObject(makePosterLists());
 
